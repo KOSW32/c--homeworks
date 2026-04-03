80d1b23b38cc1f64a806e92b8513f4e15cdb913f
KOSW32 <119171476+KOSW32@users.noreply.github.com>
2026-04-03 18:59:30 +0300

    Add files via upload

---
+++ Агрегація, композиція, наслідування.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+using namespace std;
+
+// ================== АГРЕГАЦИЯ ==================
+class EngineA {
+public:
+    void Start() {
+        cout << "EngineA started\n";
+    }
+};
+
+class CarA {
+private:
+    EngineA* engine;
+
+public:
+    CarA(EngineA* eng) {
+        engine = eng;
+    }
+
+    void Drive() {
+        engine->Start();
+        cout << "CarA is driving\n";
+    }
+};
+
+// ================== КОМПОЗИЦИЯ ==================
+class EngineB {
+public:
+    void Start() {
+        cout << "EngineB started\n";
+    }
+};
+
+class CarB {
+private:
+    EngineB engine;
+
+public:
+    void Drive() {
+        engine.Start();
+        cout << "CarB is driving\n";
+    }
+};
+
+// ================== СПАДКУВАННЯ ==================
+class Animal {
+public:
+    void Eat() {
+        cout << "Animal eats\n";
+    }
+};
+
+class Dog : public Animal {
+public:
+    void Bark() {
+        cout << "Dog barks\n";
+    }
+};
+
+// ================== ВВЫВОД ==================
+int main() {
+
+    cout << "=== Aggregation ===\n";
+    EngineA engA;
+    CarA carA(&engA);
+    carA.Drive();
+
+    cout << "\n=== Composition ===\n";
+    CarB carB;
+    carB.Drive();
+
+    cout << "\n=== Inheritance ===\n";
+    Dog dog;
+    dog.Eat();
+    dog.Bark();
+
+    return 0;
+}
